2520066509e817151d3b95ac601538e700c4d2f4
algo21-221040069 <100129194+algo21-221040069@users.noreply.github.com>
2022-05-21 23:56:30 +0800

    Update resnet.py

--- codes/strategy/resnet.py
+++ codes/strategy/resnet.py
@@ -34,15 +34,14 @@ def resnet_block(input_channels, num_channels, num_residuals, first_block=False)
             blk.append(Residual(num_channels, num_channels))
     return blk
 
-
-b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=2, padding=1), nn.BatchNorm2d(64), nn.ReLU(),
+def net():
+    b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=2, padding=1), nn.BatchNorm2d(64), nn.ReLU(),
                    nn.MaxPool2d(kernel_size=2, padding=1))
-b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
-b3 = nn.Sequential(*resnet_block(64, 128, 2))
-b4 = nn.Sequential(*resnet_block(128, 256, 2))
-b5 = nn.Sequential(*resnet_block(256, 512, 2))
-
-net = nn.Sequential(b1, b2, b3, b4, b5, nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(), nn.Linear(512, 3))
+    b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
+    b3 = nn.Sequential(*resnet_block(64, 128, 2))
+    b4 = nn.Sequential(*resnet_block(128, 256, 2))
+    b5 = nn.Sequential(*resnet_block(256, 512, 2))
+    return nn.Sequential(b1, b2, b3, b4, b5, nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(), nn.Linear(512, 3))
 
 
 def train(net, train_iter, num_epochs, lr):
