4b699c663b233ce21395e589dca3f588778fcb38
algo21-221040076 <97863208+algo21-221040076@users.noreply.github.com>
2022-05-22 11:31:52 +0800

    Update getMarketData.py

--- codes/database/getMarketData.py
+++ codes/database/getMarketData.py
@@ -34,38 +34,36 @@ class MarketData():
             return dictToReturn
         print("Market Close")
 
+if __name__=='__main__':
+
+    # mysql element
+    host='rm-wz92xxsa5315l05g2so.mysql.rds.aliyuncs.com'
+    port=3306
+    user='algo76'
+    passwd="123456@CUHKSZ"
+    db='mfe5210project'
+    conn = connect(host=host,port=port
+                    ,user=user,passwd=passwd
+                    ,db=db,charset='utf8')
+    cursor = conn.cursor()
 
 
-# mysql element
-host='rm-wz92xxsa5315l05g2so.mysql.rds.aliyuncs.com'
-port=3306
-user='algo76'
-passwd="123456@CUHKSZ"
-db='mfe5210project'
-conn = connect(host=host,port=port
-                ,user=user,passwd=passwd
-                ,db=db,charset='utf8')
-cursor = conn.cursor()
+
+    # test
+    beginTime = datetime.datetime(2022, 2, 7, 9, 30, 00)
+
+    current = ["TradingDate" , "TradingTime" , "Symbol" , "OpenPrice" , "LastPrice" , "HighPrice" , "LowPrice" , "SettlePrice" , "PreSettlePrice" , "ClosePrice" , "PreClosePrice" , "TradeVolume" , "TotalVolume" , "TradeAmount" , "TotalAmount" , "PreTotalPosition" , "TotalPosition" , "PrePositionChange" , "PriceUpLimit" , "PriceDownLimit" , "BuyOrSell" , "OpenClose" , "BuyPrice01" , "BuyPrice02" , "BuyPrice03" , "BuyPrice04" , "BuyPrice05" , "SellPrice01" , "SellPrice02" , "SellPrice03" , "SellPrice04" , "SellPrice05" , "BuyVolume01" , "BuyVolume02" , "BuyVolume03" , "BuyVolume04" , "BuyVolume05" , "SellVolume01" , "SellVolume02" , "SellVolume03" , "SellVolume04" , "SellVolume05" , "Delta" , "PreDelta" , "SettleGroupID" , "SettleID" , "Change" , "ChangeRatio" , "ContinueSign" , "Market" , "UNIX" , "PositionChange" , "ContinueSignName" , "SecurityID" , "ShortName" , "AveragePrice" , "OrderRate" , "OrderDiff" , "Amplitude" , "VolRate" , "SellVOL" , "BuyVOL"]
+    history =["TradingDate","TradingTime","Symbol","OpenPrice","ClosePrice","HighPrice","LowPrice","Change","ChangeRatio","Position","Volume","Amount"]
 
 
+    BackMD = MarketData(beginTime,conn,"Market_data_Backtest",history)
+    TradeMD = MarketData(beginTime,conn,"Market_data_Trade",current)
+    for i in list(range(100)):
+        # MarketData1 = BackMD.getMarketData()
+        MarketData2 = TradeMD.getMarketData()
 
-# test
-beginTime = datetime.datetime(2022, 2, 7, 9, 30, 00)
-
-current = ["TradingDate" , "TradingTime" , "Symbol" , "OpenPrice" , "LastPrice" , "HighPrice" , "LowPrice" , "SettlePrice" , "PreSettlePrice" , "ClosePrice" , "PreClosePrice" , "TradeVolume" , "TotalVolume" , "TradeAmount" , "TotalAmount" , "PreTotalPosition" , "TotalPosition" , "PrePositionChange" , "PriceUpLimit" , "PriceDownLimit" , "BuyOrSell" , "OpenClose" , "BuyPrice01" , "BuyPrice02" , "BuyPrice03" , "BuyPrice04" , "BuyPrice05" , "SellPrice01" , "SellPrice02" , "SellPrice03" , "SellPrice04" , "SellPrice05" , "BuyVolume01" , "BuyVolume02" , "BuyVolume03" , "BuyVolume04" , "BuyVolume05" , "SellVolume01" , "SellVolume02" , "SellVolume03" , "SellVolume04" , "SellVolume05" , "Delta" , "PreDelta" , "SettleGroupID" , "SettleID" , "Change" , "ChangeRatio" , "ContinueSign" , "Market" , "UNIX" , "PositionChange" , "ContinueSignName" , "SecurityID" , "ShortName" , "AveragePrice" , "OrderRate" , "OrderDiff" , "Amplitude" , "VolRate" , "SellVOL" , "BuyVOL"]
-history =["TradingDate","TradingTime","Symbol","OpenPrice","ClosePrice","HighPrice","LowPrice","Change","ChangeRatio","Position","Volume","Amount"]
-
-
-BackMD = MarketData(beginTime,conn,"Market_data_Backtest",history)
-TradeMD = MarketData(beginTime,conn,"Market_data_Trade",current)
-for i in list(range(100)):
-    # MarketData1 = BackMD.getMarketData()
-    MarketData2 = TradeMD.getMarketData()
-
-    # print(MarketData1)
-    print(MarketData2)
-
-
+        # print(MarketData1)
+        print(MarketData2)
 
 
 
